1c450b9456a6e4953603dcbdd928f2ccce8fc6e3
Fudong Bai <fudongbai@gmail.com>
2019-08-03 09:44:41 +0800

    rpi3b: add default kernel compiler prefix
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,7 @@ TARGET_CPU_VARIANT := cortex-a53
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_KERNEL_CONFIG := lineageos_rpi3_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-gnueabihf-
 TARGET_KERNEL_SOURCE := kernel/brcm/rpi3
 
 # Audio
